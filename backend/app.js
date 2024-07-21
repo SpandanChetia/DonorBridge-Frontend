2bec287b330576d936df6790ced74b01a6b21afd
Spandan Chetia <129678928+SpandanChetia@users.noreply.github.com>
2024-07-21 15:45:58 +0530

    Delete backend directory

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const { application } = require('express');
-
-const app = express();
-
-app.set('view engine', 'pug');
-app.set('view', 'views');
